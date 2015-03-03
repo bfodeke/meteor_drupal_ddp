dadbf202d0e7e42da11f220f2c608de51d79339f
Bayo Fodeke <bayof@knowclassic.com>
2015-03-03 16:02:32 -0500

    Added user fixture

---
+++ server/fixtures/users.fixtures.js
@@ -0,0 +1,14 @@
+if ( Meteor.users.find().count() === 0 ) {
+    Accounts.createUser({
+        username: 'admin',
+        email: 'admin@example.com',
+        password: 'password',
+        profile: {
+            first_name: 'Bayo',
+            last_name: 'Fodeke',
+            company: 'Classic',
+        }
+    });
+
+    Meteor.users.update("username":"admin", {$set: {"emails.0.verified" :true}});
+}
