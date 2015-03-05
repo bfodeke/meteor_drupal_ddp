363cd4a92809a6f053ca153c1352f9a9b6f62bb1
Bayo Fodeke <bayof@knowclassic.com>
2015-03-05 13:36:51 -0500

    removed test code

--- client/views/templates/nodes/teaser/nodeTeaser.js
+++ client/views/templates/nodes/teaser/nodeTeaser.js
@@ -1,7 +1,6 @@
 Template.nodeTeaser.helpers({
 	// console.log(this);
 	thumb: function(){
-		console.log(this);
 		if ($.type(this.content.field_image) === 'object') {
 			return 'http://drupalddp.dev/sites/default/files/' + this.content.field_image.und[0].filename;
 		}else{
@@ -11,10 +10,3 @@ Template.nodeTeaser.helpers({
 	// thumb : 'http://drupalddp.dev/sites/default/files/' + this.data.content.field_image.und[0].filename
 	// }
 });
-
-Template.nodeTeaser.rendered = function(){
-	// this.data.thumb = 'http://drupalddp.dev/sites/default/files/' + this.data.content.field_image.und[0].filename;
-	// console.log(this.data.content.field_image.und[0].filename);
-	// console.log($.type(this.data.content.field_image));
-	console.log(this);
-};
