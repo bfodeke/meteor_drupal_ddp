c96c4227830d2ff30d985a5810a7eb128f9d8bb2
Bayo Fodeke <bayof@knowclassic.com>
2015-03-05 20:48:30 -0500

    added animations using _uihooks

--- client/views/templates/nodes/nodeList.js
+++ client/views/templates/nodes/nodeList.js
@@ -3,3 +3,34 @@ Template.nodeList.helpers({
     return drupalDdpNodes.find({},{sort: {"content.created": -1}});
   }
 });
+
+Template.nodeList.rendered = function(){  
+  this.$('.list-of-nodes').get(0)._uihooks = {
+    insertElement: function(node, next){
+      $(node).addClass('animated slideInDown');
+      $(node).insertBefore(next);
+
+      Meteor.setTimeout(function() {
+        $(node).removeClass('animated slideInDown');
+      },500);
+    },
+
+    moveElement: function(node){
+      $(node).addClass('animating');
+
+      Meteor.setTimeout(function() {
+        $(node).removeClass('animating');
+      },500);
+    },
+
+    removeElement: function(node){
+      $(node).addClass('animated slideOutUp');
+
+      Meteor.setTimeout(function() {
+        $(node).remove();
+      }, 700);
+    }
+
+    //TODO: use velocityjs for animation
+  };
+}
